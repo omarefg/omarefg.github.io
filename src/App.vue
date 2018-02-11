7782210f359bb8a3a0e1b4920d2b4300ffc1232f
Omar Flores <omarefg92@gmail.com>
2018-02-11 17:28:53 -0500

    Initial Commit, en este punto no tengo mucho, estoy integrando el header

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  #app
+    of-header
+</template>
+
+<script>
+import OfHeader from '@/components/layout/Header'
+
+export default {
+  name: 'app',
+
+  components: { OfHeader },
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './css/index.css';
+  @import './scss/main.scss';
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  header
+    .container.is-fluid.navbar
+      .navbar-menu.columns
+        .navbar-start.column
+          .logo
+            a(href="index.html")
+              img(src="@/assets/logo.png" alt="logo" height="95")
+              h1.letra_logo OF
+        .lista.navbar-end.column#lista
+          ul.menu-list
+            li
+              a(href="blog.html")
+                b BLOG
+            li
+              a(href="mailto:omarefg92@gmail.com")
+                b CONTACT
+            li
+              a.redes_sociales#redes_sociales(touchstart="menuCan()")
+                b SOCIAL
+              ul.social.menu-list#social
+                li
+                  a(href="https://www.platzi.com/@omarefg" target="_blank")
+                    i.jjy-platzi
+                li
+                  a(href="https://codepen.io/omarefg/" target="_blank")
+                    i.jjy-codepen
+                li
+                  a(href="https://www.linkedin.com/in/omarefg/" target="_blank")
+                    i.jjy-linkedin
+                li
+                  a(href="https://www.twitter.com/omarefg" target="_blank")
+                    i.jjy-twitter
+                li
+                  a(href="https://github.com/omarefg" target="_blank")
+                    i.jjy-github
+                li
+                  a(href="https://www.instagram.com/omarefg92/" target="_blank")
+                    i.fth-instagram
+    .banner
+      img(src="@/assets/banner - copia2.jpg" alt="header" width="1349")
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .menu-list{
+    border: none !important;
+  }
+</style>
