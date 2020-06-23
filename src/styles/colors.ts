680b851731d441cced305e52a3b6d61f7be81f9f
Omar Flores Grimontt <dev@omarefg.com>
2020-06-22 22:12:29 -0500

    Improved way colors where served

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -3,22 +3,34 @@ interface palette {
   sideBarBackgroundColor: string,
   textColor: string
 }
+interface theme {
+  light: palette,
+  dark: palette
+}
+type mode = 'dark' | 'light'
+
+function getMode ():mode {
+  if (localStorage.getItem('colorMode') === 'dark') {
+    return 'dark'
+  }
+  return 'light'
+}
+
+const themes:theme = {
+  light: {
+    backgroundColor: '#f7f7f7',
+    sideBarBackgroundColor: '#eee',
+    textColor: '#323330'
+  },
+  dark: {
+    backgroundColor: '#323330',
+    sideBarBackgroundColor: '#444',
+    textColor: '#f7f7f7'
+  }
+}
 
 export function getPalette ():palette {
-  const colorMode = localStorage.getItem('colorMode')
-  let backgroundColor = '#f7f7f7'
-  let sideBarBackgroundColor = '#eee'
-  let textColor = '#323330'
-
-  if (colorMode === 'dark') {
-    backgroundColor = '#323330'
-    sideBarBackgroundColor = '#444'
-    textColor = '#f7f7f7'
-  }
-
   return {
-    backgroundColor,
-    sideBarBackgroundColor,
-    textColor
+    ...themes[getMode()]
   }
 }
